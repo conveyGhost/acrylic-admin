ec1725e2c09b7d4465494d05c651fb202d5f6b48
codeninja <panda95718@gmail.com>
2024-01-10 11:45:16 +0900

    feat: Create authorization pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import Login from '../pages/Auth/Login';
+import SignUp from "../pages/Auth/SIgnUp";
+import { AuthLayout } from "../layouts/AuthLayout";
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Dashboard } from "../pages/Dashboard";
 
 export const Router = () => {
   return (
     <Routes>
+      <Route path="auth" element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Route>
       <Route path="" element={<DefaultLayout />}>
- 
         <Route index element={<Dashboard />} />
         <Route path='dashboard' element={<Dashboard />} />
-
       </Route>
 
     </Routes>
