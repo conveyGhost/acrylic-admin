893604791b528b9d3c225eff38eaa6a1f7874430
Ingemar Lundgren <155203838+conveyGhost@users.noreply.github.com>
2024-07-12 12:05:39 +0900

    conveyGhost fix: some title changed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 export const Dashboard = () => {
     return (
-        <div className="text-red-500 py-10">This is Acrylic Admin Page</div>
+        <div className="text-red-500 py-10">This is Acrylic Admin</div>
     )
 }
